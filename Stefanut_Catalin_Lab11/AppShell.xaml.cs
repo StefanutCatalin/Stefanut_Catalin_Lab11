19f2e52ba0baeeb313b1dd83357d15dcf74de0a0
StefanutCatalin <stefanut.catalin420@gmail.com>
2022-12-20 20:28:00 +0200

    Add project files.

---
+++ Stefanut_Catalin_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Stefanut_Catalin_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Stefanut_Catalin_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using Stefanut_Catalin_Lab11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stefanut_Catalin_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }   
+}
